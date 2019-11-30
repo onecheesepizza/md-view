1b0ce0c9d189a121d99dee334d00374b3672a003
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2019-11-29 18:23:56 -0800

    add basic functionality

---
+++ controllers/indexController.js
@@ -0,0 +1,33 @@
+// Handle Author update on POST.
+const axios = require('axios');
+const showdown = require("showdown");
+const converter = new showdown.Converter();
+//enable automatic URL linking etc via github flavor
+converter.setFlavor('github');
+converter.setOption('ghCodeBlocks', true);
+
+exports.index = function(req, res, next) {
+    //get file URL from request
+    const mdFileURL = req.query.url;
+    console.log("URL requested:", mdFileURL);
+    console.log("getting file from URL...");
+    //get data from URL
+    axios
+    .get(mdFileURL)
+    .then(function(response) {
+        // handle success
+        console.log("file received.");
+        
+        // convert Markdown to HTML
+        const markdownHTML = converter.makeHtml(response.data);
+        console.log("MD converted to HTML.");
+
+        // render page
+        res.render('index', {mdHTML: markdownHTML});
+        console.log("HTML response sent.");
+    })
+    .catch(function(error) {
+        // handle error
+        console.log(error);
+    });
+};
