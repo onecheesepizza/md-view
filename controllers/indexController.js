f836c707cd85711ab5f6e96a54c04e8f004ad3cb
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2019-11-30 01:38:18 -0800

    add input form

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -13,35 +13,43 @@ exports.index = function(req, res, next) {
     // get file URL from request
     let mdFileURL = req.query.url;
 
-    // append &dl=1 to file if needed to allow raw Dropbox files
-    if (mdFileURL.slice(mdFileURL.length-3) === ".md") {
-        mdFileURL = mdFileURL + "?dl=1";
-    } else if (mdFileURL.slice(mdFileURL.length-8) === ".md?dl=0") {
-        mdFileURL = (mdFileURL.slice(0, mdFileURL.length-8)) + ".md?dl=1";
-    }
-
-    // get data from URL
-    console.log("getting file from:", mdFileURL);
-    axios
-    .get(mdFileURL)
-    .then(function(response) {
-        // handle success
-        console.log("file received.");
-        
-        // convert Markdown to HTML
-        const markdownHTML = converter.makeHtml(response.data);
-        console.log("MD converted to HTML.");
-
-        // render page
-        res.render('index', {
-            mdHTML: markdownHTML,
-            title: 'md-view',
-            highlightStyle: 'highlight.js/styles/atom-one-dark.css'
+    // render input form if no file provided in URL
+    if (!mdFileURL) {
+        res.render('url-input', {
+            pageStyle: 'form-reset.css'
         });
-        console.log("HTML response sent.");
-    })
-    .catch(function(error) {
-        // handle error
-        console.log(error);
-    });
+    // render markdown if file provided in URL  
+    } else {
+        // append &dl=1 to file if needed to allow raw Dropbox files
+        if (mdFileURL.slice(mdFileURL.length-3) === ".md") {
+            mdFileURL = mdFileURL + "?dl=1";
+        } else if (mdFileURL.slice(mdFileURL.length-8) === ".md?dl=0") {
+            mdFileURL = (mdFileURL.slice(0, mdFileURL.length-8)) + ".md?dl=1";
+        }
+
+        // get data from URL
+        console.log("getting file from:", mdFileURL);
+        axios
+        .get(mdFileURL)
+        .then(function(response) {
+            // handle success
+            console.log("file received.");
+            
+            // convert Markdown to HTML
+            const markdownHTML = converter.makeHtml(response.data);
+            console.log("MD converted to HTML.");
+
+            // render page
+            res.render('index', {
+                mdHTML: markdownHTML,
+                title: 'md-view',
+                highlightStyle: 'highlight.js/styles/atom-one-dark.css'
+            });
+            console.log("HTML response sent.");
+        })
+        .catch(function(error) {
+            // handle error
+            console.log(error);
+        });
+    }
 };
