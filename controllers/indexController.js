8b1d6985328e3d810daa95a1020814fbfbb58a6f
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2019-11-30 20:32:05 -0800

    fix input validation

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -12,19 +12,26 @@ converter.setOption('ghCodeBlocks', true);
 exports.index = function(req, res, next) {
     // get file URL from request
     let mdFileURL = req.query.url;
-
-    // render input form if no file provided in URL, or if file in URL does not end with .md 
-    if (!mdFileURL || mdFileURL.slice(mdFileURL.length-3) != '.md') {
+    // console.log(mdFileURL.slice(-8, -5));
+    // render input form if... 
+    if ( 
+        //no url 
+        !mdFileURL 
+        // url does not end in .md or .md?dl=x
+        ||  (mdFileURL.slice(-3) != '.md' && mdFileURL.slice(-8, -5) != '.md') 
+        //url does not start with http
+        || mdFileURL.slice(0,4) !='http'
+        ) {
         res.render('url-input', {
             pageStyle: 'form-reset.css'
         });
     // render markdown if .md file provided in URL  
-    } else {
-        // append &dl=1 to file if needed to allow raw Dropbox files
-        if (mdFileURL.slice(mdFileURL.length-3) === '.md') {
+    } else if (mdFileURL.slice(-3) === '.md' || mdFileURL.slice(-8,-5) === '.md'){
+        // append &dl=1 to file if needed to allow Dropbox share links
+        if (mdFileURL.slice(-3) === '.md') {
             mdFileURL = mdFileURL + "?dl=1";
-        } else if (mdFileURL.slice(mdFileURL.length-8) === '.md?dl=0') {
-            mdFileURL = (mdFileURL.slice(0, mdFileURL.length-8)) + '.md?dl=1';
+        } else if (mdFileURL.slice(-8) === '.md?dl=0') {
+            mdFileURL = (mdFileURL.slice(0, -8)) + '.md?dl=1';
         }
 
         // get data from URL
