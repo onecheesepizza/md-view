437bdbffb6a76d3979131ae37866e29c38953dfd
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2019-11-30 18:30:32 -0800

    add condition for non-md file

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,7 @@
 // Handle Author update on POST.
 const axios = require('axios');
-const showdown = require("showdown");
-const showdownHighlight = require("showdown-highlight")
+const showdown = require('showdown');
+const showdownHighlight = require('showdown-highlight')
 const converter = new showdown.Converter({
     extensions: [showdownHighlight]
 });
@@ -13,31 +13,31 @@ exports.index = function(req, res, next) {
     // get file URL from request
     let mdFileURL = req.query.url;
 
-    // render input form if no file provided in URL
-    if (!mdFileURL) {
+    // render input form if no file provided in URL, or if file in URL does not end with .md 
+    if (!mdFileURL || mdFileURL.slice(mdFileURL.length-3) != '.md') {
         res.render('url-input', {
             pageStyle: 'form-reset.css'
         });
-    // render markdown if file provided in URL  
+    // render markdown if .md file provided in URL  
     } else {
         // append &dl=1 to file if needed to allow raw Dropbox files
-        if (mdFileURL.slice(mdFileURL.length-3) === ".md") {
+        if (mdFileURL.slice(mdFileURL.length-3) === '.md') {
             mdFileURL = mdFileURL + "?dl=1";
-        } else if (mdFileURL.slice(mdFileURL.length-8) === ".md?dl=0") {
-            mdFileURL = (mdFileURL.slice(0, mdFileURL.length-8)) + ".md?dl=1";
+        } else if (mdFileURL.slice(mdFileURL.length-8) === '.md?dl=0') {
+            mdFileURL = (mdFileURL.slice(0, mdFileURL.length-8)) + '.md?dl=1';
         }
 
         // get data from URL
-        console.log("getting file from:", mdFileURL);
+        console.log('getting file from:', mdFileURL);
         axios
         .get(mdFileURL)
         .then(function(response) {
             // handle success
-            console.log("file received.");
+            console.log('file received.');
             
             // convert Markdown to HTML
             const markdownHTML = converter.makeHtml(response.data);
-            console.log("MD converted to HTML.");
+            console.log('MD converted to HTML.');
 
             // render page
             res.render('index', {
@@ -45,7 +45,7 @@ exports.index = function(req, res, next) {
                 title: 'md-view',
                 highlightStyle: 'highlight.js/styles/atom-one-dark.css'
             });
-            console.log("HTML response sent.");
+            console.log('HTML response sent.');
         })
         .catch(function(error) {
             // handle error
