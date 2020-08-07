1ca9f89cdba5d666cd2ba3e3d5450250335a1121
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2020-08-06 18:51:11 -0700

    add favicon, reorganize build file copy

--- build.js
+++ build.js
@@ -1,21 +1,34 @@
 const fs = require('fs-extra');
 
-//files to copy on build
-const buildFiles = [
+//node_modules to copy on build
+const nodeModules = [
     './node_modules/github-markdown-css',
     './node_modules/highlight.js',
-    './node_modules/clipboard/dist',
-    './src/stylesheets',
+    './node_modules/clipboard/dist'
+];
+//files to copy on build
+const srcAssetFiles = [
+    'stylesheets',
+    'favicon.png'
 ];
 
 //create output folder
 fs.emptyDir('build', (err) => {
     if (err) throw err;
-    buildFiles.forEach(file => {
+    //copy node modules
+    nodeModules.forEach(file => {
         //copy files
         fs.copy('' + file, './build/' + file, (err) => {
             if (err) throw err;
             console.log(file, 'was copied to /build/',);
         });
-    })
+    });
+    //copy asset files
+    srcAssetFiles.forEach(file => {
+        //copy files
+        fs.copy('./src/' + file, './build/' + file, (err) => {
+            if (err) throw err;
+            console.log(file, 'was copied to /build/',);
+        });
+    });
 });
